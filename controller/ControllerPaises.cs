911782858c6adb73ed8e4aea3c6a063970146bb3
FABIAN ESTEBAN LOPEZ ARIAS <fabian_esteban.lopez@uao.edu.co>
2024-05-02 19:35:02 -0500

    [Update] Prestamos.

---
+++ controller/ControllerPaises.cs
@@ -0,0 +1,45 @@
+﻿using Libreria.Models;
+using LibreriaConnection.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.controller
+{    // Luis
+    internal class ControllerPaises
+    {
+        internal List<Paises> SelectPais()
+        {
+            List<Paises> listaP = null;
+            ConnectDB objCBD = new ConnectDB();
+            string sql = "select * from paises;";
+            listaP = objCBD.SelectPais(sql);
+            return listaP;
+        }
+
+        internal bool UpdatePaises(Paises objPaises)
+        {
+            bool result = false;
+
+            string sql = "update paises set nombrePais='" + objPaises.NombrePais + "' WHERE idPais=" + objPaises.IdPais;
+
+            ConnectDB objCBD = new ConnectDB();
+            result = objCBD.ExecuteQuery(sql);
+
+            return result;  
+        }
+
+        internal List<Paises> SelectPaisC()
+        {
+            List<Paises> listaPC = null;
+            
+            string sql = "select * from paises;";
+            Paises objP = new Paises();
+            listaPC=objP.SelectPaisC(sql);
+            
+            return listaPC;
+        }
+    }
+}
--- models/Prestamos.cs
+++ models/Prestamos.cs
@@ -13,31 +13,6 @@ namespace LibreriaConnection.models
         private string fechaDevolucion;
         private bool estaVencido;
         private int idCuentaPrestamo;
-        private int idLibroPrestamo;
-
-        public Prestamos()
-        {
-        }
-
-        public Prestamos(string fechaPrestamo)
-        {
-            this.fechaPrestamo = fechaPrestamo;
-        }
-
-        public Prestamos(int idPrestamo, string fechaPrestamo)
-        {
-            this.idPrestamo = idPrestamo;
-            this.fechaPrestamo = fechaPrestamo;
-        }
-
-        public Prestamos(int idPrestamo, string fechaPrestamo, string fechaDevolucion, bool estaVencido, int idCuentaPrestamo, int idLibroPrestamo)
-        {
-            this.idPrestamo = idPrestamo;
-            this.fechaPrestamo = fechaPrestamo;
-            this.fechaDevolucion = fechaDevolucion;
-            this.estaVencido = estaVencido;
-            this.idCuentaPrestamo = idCuentaPrestamo;
-            this.idLibroPrestamo = idLibroPrestamo;
-        }
+       
     }
 }
